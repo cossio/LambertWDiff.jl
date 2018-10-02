efb244ce32b92f77de2ee2c2c3c8e8685ab6d618
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-10-02 17:33:25 -0400

    tests

--- test/lambertw_diff.jl
+++ test/lambertw_diff.jl
@@ -1,5 +1,8 @@
 using ForwardDiff, LambertW
 
-@test ForwardDiff.derivative(lambertw, 0) ≈ 1
-@test ForwardDiff.derivative(lambertw, 1) ≈ 0.361896256634889221477592614480119685556
-@test ForwardDiff.derivative(lambertw, 2) ≈ 0.230109837492917245524832192843797742125
+@testset "derivative of lambertw" begin
+    @test ForwardDiff.derivative(lambertw, 0) ≈ 1
+    @test ForwardDiff.derivative(lambertw, 1) ≈ 0.361896256634889221477592614480119685556
+    @test ForwardDiff.derivative(lambertw, 2) ≈ 0.230109837492917245524832192843797742125
+    @test ForwardDiff.derivative(lambertw, 10) ≈ 0.0635771334693451051420213110107808876419
+end
